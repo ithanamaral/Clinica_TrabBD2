2164087a2f94258c29107124161ffe7057fb54a1
tulio <tulio89lopes@gmail.com>
2026-01-12 14:57:23 -0300

    refactor: atualiza modelo de medico com dois atributos, especialidade e descricao

--- src/models/MedicoModel.js
+++ src/models/MedicoModel.js
@@ -1,10 +1,12 @@
 const Usuario = require("./UsuarioModel");
 
 class Medico extends Usuario {
-    constructor(nome, cpf, email, senha, dataNasc, endereco, numero, uf, crm) {
+    constructor(nome, cpf, email, senha, dataNasc, endereco, numero, uf, crm, especialidade, descricao) {
         super(nome, cpf, 'MEDICO', email, senha, dataNasc, endereco, numero);
         this.uf = uf;
         this.crm = crm;
+        this.especialidade = especialidade;
+        this.descricao = descricao;
     }
 }
 
